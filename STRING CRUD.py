e1e8e4e5e9e2d213c4bcb750d0d5e8b0490c12b7
rumanahamed <93419782+rumanahamed@users.noreply.github.com>
2021-11-03 18:55:03 +0530

    Add files via upload

---
+++ STRING CRUD.py
@@ -0,0 +1,14 @@
+#create
+str = 'hello' #single quotation
+str1 = "hello2" #double quotation
+str2 = "'hello3'" #tripe quotation
+
+#Read/access - It can be done by indexing
+print(str[0]) #positive indexing
+print(str[-3]) #negative indexing
+print(str[0:2]) #print multiple substring by  using range
+
+#update - We can't update tuple as it is immutable, however, updation can be done with the help of concatination & typecasting
+#delete
+del str # removing an item or deleting an item is not possible, we can only delete entire tuple.
+

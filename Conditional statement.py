fd74d9ad2292fe08653c05409037987133ad7d81
rumanahamed <93419782+rumanahamed@users.noreply.github.com>
2021-11-05 17:42:14 +0530

    Add files via upload

---
+++ Conditional statement.py
@@ -0,0 +1,13 @@
+x = 21
+# if x%2==0:
+#     print('Number is even',x)
+# else:
+#     print('number is odd',x)
+if x%2==0 and x%7==0:
+    print('Number is even and divisible by 7=',x)
+elif x%5==0:
+    print('Number is divisible by 5=',x)
+else:
+    print(x)
+# elif x%7==0:
+#     print('Number is divisible by 7=',x)
